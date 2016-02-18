627c93770bb05b00d4b7ad5f496f8e5bc80cba7e
Kidist Seifu Gragne <kidistsg@kidist.uio.no>
2016-02-18 14:31:00 +0100

    accept user input and print out it

---
+++ get_os_number.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""
+Created on 2016-02-18
+Accept two user arguments and print out the arguments value 
+@author kidist
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-version", required = True, help = "os-x version")
+parser.add_argument('-f', default = "db.sqlite", help = "filename path")
+args = parser.parse_args()
+print args.version
+print args.f
