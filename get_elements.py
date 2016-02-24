bf8cb3d579c60a1c63bc5be6c6ac13a17a360f6a
Kidist Seifu Gragne <kidistsg@kidist.uio.no>
2016-02-24 09:09:11 +0100

    lists 10 hostname and serial_number from machine table

--- get_elements.py
+++ get_elements.py
@@ -6,31 +6,7 @@ db = sqlite3.connect('munkireport-db.sqlite')
 cursor =  db.cursor()
 cursor.execute ("SELECT hostname,serial_number FROM machine")
 
-all_rows = cursor.fetchall()
+all_rows = cursor.fetchmany(size=10)
 for row in all_rows:
-	print('{0}:{1}'.format(row[0],row[1]))
-	#print('{0}:{1},{2},{3},{4},{5},{6},{7},{8},{9}'.format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]))
+	print '%10s %10s' % (row[0],row[1])
 db.close()
-
-"""
-import glob
-import sys
-
-path = '/home/python_oppgave/munkireport-db.sqlite'
-files = glob.glob(path)
-
-for file in files:
-  f = open(file,'r')
-  print '%s' % f.readlines()
-#  sys.stdout.write(f.read())
-  f.close()
-"""
-
-"""
-import os
-
-#os.getcwd()
-myfile = open('/home/python_oppgave/munkireport-db.sqlite')
-mytext = myfile.read()
-myfile.close
-"""
