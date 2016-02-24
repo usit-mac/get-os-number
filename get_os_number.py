7abf579284ff679155ebc33cadc05d373b02132d
Kidist Seifu Gragne <kidistsg@kidist.uio.no>
2016-02-24 13:25:06 +0100

    list out the number of machine from DB for a given OS X-version

--- get_os_number.py
+++ get_os_number.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python
 
 """
-Created on 2016-02-18
+Created on 2016-02-24
 Accept two user arguments and print out the arguments value 
-@author kidistsg
+@author kidist
 """
-
+import sqlite3
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-version", required = True, help = "os-x version")
-parser.add_argument('-f', default = "db.sqlite", help = "filename path")
+parser.add_argument("-v", required = True, help = "os-x version")
+parser.add_argument('-f', default = "munkireport-db.sqlite", help = "filename path")
+
 args = parser.parse_args()
-print args.version
-print args.f
+
+db = sqlite3.connect(args.f)
+cursor = db.cursor()
+
+cursor.execute ("SELECT COUNT(*) FROM machine WHERE os_version=?", (args.v,))
+
+print cursor.fetchone()[0]
