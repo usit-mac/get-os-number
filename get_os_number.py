257ec96dbcdc2128695e17ad08c576745169c396
Kidist Seifu Gragne <kidistsg@kidist.uio.no>
2016-02-25 13:38:30 +0100

    List out unique OS X-versions from the DB

--- get_os_number.py
+++ get_os_number.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python
 
 """
-Created on 2016-02-24
+Created on 2016-02-25
 Accept two user arguments and print out the arguments value 
 @author kidist
 """
-import sqlite3
+
 import argparse
+import sqlite3
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", '--version', required = True, nargs = '*', help = "list of os-x version")
-parser.add_argument('-f', '--filename', default = "munkireport-db.sqlite", help = "filename path")
-
+parser.add_argument('-f','--filename', default = "munkireport-db.sqlite", help = "filename path")
 args = parser.parse_args()
 
-db = sqlite3.connect(args.filename)
-cursor = db.cursor()
-for arg in args.version:
-        print "OS X-versio %s" % arg
-        cursor.execute ("SELECT COUNT(*) FROM machine WHERE os_version=?", (arg,))
-        print cursor.fetchone()[0]
+def unique_version( filename ):
+	db = sqlite3.connect(filename)
+	cursor =  db.cursor()
+	cursor.execute ("SELECT DISTINCT os_version FROM machine")
+	all_versions = cursor.fetchall()
+
+	db.close()
+
+	lists = []
+
+	for version in all_versions:
+		if version[0] !=0 and version[0]!=None:
+			lists.append(version[0])
+	return lists
+print unique_version(args.filename)
