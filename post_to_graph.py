8f14543042d42231e2c526f10b50fc13d8491867
Kidist Seifu Gragne <kidistsg@kidist.uio.no>
2016-03-02 10:47:11 +0100

    editing the name of unique_versios(), post_to_graphite() arguments and move the for loop to _main_ function

--- post_to_graph.py
+++ post_to_graph.py
@@ -11,37 +11,36 @@ import time
 import argparse
 import platform
 
-def unique_version(filename):
-	db = sqlite3.connect(filename)
-	cursor =  db.cursor()
-	cursor.execute ("SELECT DISTINCT os_version FROM machine")
-	all_versions = cursor.fetchall()
+def unique_versions(filename):
+    db = sqlite3.connect(filename)
+    cursor =  db.cursor()
+    cursor.execute ("SELECT DISTINCT os_version FROM machine")
+    all_versions = cursor.fetchall()
+	
+    return all_versions
+    db.close()
 
-	db1 = sqlite3.connect(filename)
-        cursor1 =  db1.cursor()
+def post_to_graphite(metric,value,server='collected-prod02.uio.no',port=2003):
+    timestamp = int(time.time())
+    message = '%s %s %d' % (value, metric,timestamp)
 
-	for version in all_versions:
-		if version[0] !=0 and version[0]!=None:
-			for version in all_versions:
-				cursor1.execute ("SELECT COUNT(*) FROM machine WHERE os_version=?", (version[0],))
-				totalhost = cursor1.fetchone()[0]
-			post_to_graphite(totalhost, version, server='collected-prod02.uio.no', port=2003)
-	db1.close()
-
-
-def post_to_graphite(metric, value,server, port):
-	timestamp = int(time.time())
-	message = '%s %s %d\n' % (value, metric,timestamp)
-
-	print 'sending message:\n%s' % message
-	sock = socket.socket()
-	sock.connect((server, port))
-	sock.sendall(message)
-	sock.close()
+    print 'Resolution.daily.mac.client.os.%s' % message
+    sock = socket.socket()
+    sock.connect((server, port))
+    sock.sendall(message)
+    sock.close()
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-f','--filename', default = "munkireport-db.sqlite", help = "filename path")
-	args = parser.parse_args()
-	unique_version(args.filename)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--filename', default = "munkireport-db.sqlite", help = "filename path")
+    args = parser.parse_args()
+
+    for version in unique_versions(args.filename):
+        if version[0] !=0 and version[0]!=None:
+            db = sqlite3.connect(args.filename)
+            cursor = db.cursor()
+	    cursor.execute ("SELECT COUNT(*) FROM machine WHERE os_version=?", (version[0],))
+            totalhost = cursor.fetchone()[0]
+            post_to_graphite(totalhost, version)
+	    db.close()
 
