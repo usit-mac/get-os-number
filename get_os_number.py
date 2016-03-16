bb51442795c3c42c1b7ec8f2323997432b2fdf23
Kidist Seifu Gragne <kidistsg@kidist.uio.no>
2016-03-16 12:14:40 +0100

    Accept 4 arguments and connect MySQL-DB and manipulate the data

--- get_os_number.py
+++ get_os_number.py
@@ -10,13 +10,13 @@ post it to a collectd server.
 import pymysql
 import socket
 import time
-
+import argparse
 
 def unique_versions(dbname):
     """
     Returns all unique OS-version from MySql database 
     """
-    with pymysql.connect(host = 'localhost', user = 'root', db = dbname, passwd = '') as cursor:
+    with pymysql.connect(host = args.hostname, user = args.username, db = dbname, passwd = args.passwd) as cursor:
         cursor.execute ("SELECT DISTINCT os_version FROM machine")
         all_versions = cursor.fetchall()
 
@@ -36,28 +36,37 @@ def post_to_graphite(metric, value, server='collected-prod02.uio.no', port=2003)
     timestamp = int(time.time())
     message = '%s %s %d' % (value, metric, timestamp)
 
-#    sock = socket.socket()
-#    sock.connect((server, port))
-#    sock.sendall(message)
-#    sock.close()
+    sock = socket.socket()
+    sock.connect((server, port))
+    sock.sendall(message)
+    sock.close()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-H','--hostname', default = 'localhost',help="FQDN of the host.")
+    parser.add_argument('-u','--username', default = 'root', help= "Database administrator user name")
+    parser.add_argument('-db','--dbname', default = 'my_database', help = "Name of the required database")
+    parser.add_argument('-p','--passwd', default ='', help = "DB administraor password")
+    parser.add_argument('--post-to-graphite', action='store_true', help="Switch for posting the data to graphite.")
+    parser.add_argument('-v', '--verbose',action = 'store_true', help = "Verbose printing")
+
+    args = parser.parse_args()
 
     metric_base = 'resolution.daily.mac.clients.os.%s'
-
-    with pymysql.connect(host = 'localhost', user = 'root', db = 'my_database', passwd = '') as cursor:
-        db_name = 'my_database'
-        for version in unique_versions(db_name):
+    
+    with pymysql.connect(host = args.hostname, user = args.username, db = args.dbname, passwd = args.passwd) as cursor:
+        for version in unique_versions(args.dbname):
             cursor.execute ("SELECT COUNT(*) FROM machine WHERE os_version=%s", (version,))
             total_clients = cursor.fetchone()[0]
 
-            if post_to_graphite:
+            if args.post_to_graphite:
                 metric = metric_base % version
 
-#                if args.verbose:
-                print '  Graphite: %s %d' % (metric, total_clients)
+                if args.verbose:
+	                print '  Graphite: %s %d' % (metric, total_clients)
 
                 post_to_graphite(metric=metric, value=total_clients)
 
-#            print '%s %d' % (version, total_clients)
+            print '%s %d' % (version, total_clients)
 
